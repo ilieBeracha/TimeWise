f522bc5e8481b24a2221c7afb0ff91e77b836ce6
ilieBeracha <ilieberacha123@gmail.com>
2023-04-04 18:53:54 +0300

    sketch

--- timewise/src/pages/LandingPage/LandingPage.tsx
+++ timewise/src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,17 @@
 import "./LandingPage.css";
 import logo from "../../assets/images/mainLogo.png";
+import image2 from "../../assets/images/logo.png";
 import Register from "../../Components/Register/Register";
 import Login from "../../Components/Login/Login";
-import background from '../../assets/images/background2.jpg'
+import bash from '../../assets/images/bash.svg'
+import clients_main from '../../assets/images/clients-main.svg'
 
 import { useRef } from "react";
+import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function LandingPage(): JSX.Element {
-    const ref: any = useRef()
+    const parallax = useRef<IParallax>(null!)
 
-
-    function goto() {
-        location.href = '#LandingPageAboutUs'
-    }
     return (
         <div className="LandingPage">
 
@@ -23,71 +22,101 @@ function LandingPage(): JSX.Element {
                 </div>
 
                 <div className="LandingPageHeaderLinks">
-                    <a href="#LandingPageWelcome">Welcome</a>
-                    <a href="#LandingPageAboutUs">About us</a>
-                    <a href="#LandingPageWebsites">More</a>
                     <Register />
                     <Login />
                 </div>
             </div>
 
-            {/* 1 */}
+            <Parallax pages={3} ref={parallax} style={{ top: 0, left: 0 }}>
 
-            <div id="LandingPageWelcome" className="LandingPageWelcome">
-                <h1>Welcome to <span>TimeWise</span> </h1>
+                {/* 1 */}
 
-                {/* <p>Personalized travel planning for unforgettable adventures.</p> */}
-                <button onClick={goto} className="button-51">Learn more</button>
-            </div>
+                <ParallaxLayer
+                    offset={0}
+                    speed={2}
+                    factor={1}
+                    onClick={() => parallax.current.scrollTo(1)}
+                >
+                    <div id="LandingPageWelcome" className="LandingPageWelcome">
+                        <h1>Welcome to <span>TimeWise</span> </h1>
 
-            {/* 2 */}
+                        <p>Personalized travel planning for unforgettable adventures.</p>
+                        {/* <button onClick={goto} className="button-51">Learn more</button> */}
+                    </div>
+                </ParallaxLayer>
 
-            <div id="LandingPageAboutUs" className="LandingPageAboutUs">
-                <div className="LandingPageAboutUsHeader">
-                    <h1>About us</h1>
-                    <div className="Marker"></div>
-                </div>
-                <div className="LandingPageAboutUsFeatures">
-                    <div className="LandingPageAboutUsFeature">
-                        <div className="LandingPageAboutUsFeatureHeader">
+                <ParallaxLayer offset={0.95} speed={0.8} style={{ opacity: 0.7 }}>
+                    <img src={bash} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+                    <img src={bash} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+                </ParallaxLayer>
 
+                {/* 2 */}
+
+                <ParallaxLayer
+                    offset={1}
+                    speed={1}
+                    factor={1}
+                    onClick={() => parallax.current.scrollTo(2)}
+                >
+
+
+                    <div id="LandingPageAboutUs" className="LandingPageAboutUs">
+                        <div className="LandingPageAboutUsHeader">
+                            <h1>About us</h1>
+                            <div className="Marker"></div>
                         </div>
-                        <div className="LandingPageAboutUsFeatureContent">
+                        <div className="LandingPageAboutUsFeatures">
+                            <div className="LandingPageAboutUsFeature">
+                                <div className="LandingPageAboutUsFeatureHeader">
 
+                                </div>
+                                <div className="LandingPageAboutUsFeatureContent">
+
+                                </div>
+                            </div>
+                            <div className="LandingPageAboutUsFeature">
+                                <div className="LandingPageAboutUsFeatureHeader">
+
+                                </div>
+                                <div className="LandingPageAboutUsFeatureContent">
+
+                                </div>
+                            </div>
+                            <div className="LandingPageAboutUsFeature">
+                                <div className="LandingPageAboutUsFeatureHeader">
+
+                                </div>
+                                <div className="LandingPageAboutUsFeatureContent">
+
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="LandingPageAboutUsFeature">
-                        <div className="LandingPageAboutUsFeatureHeader">
+                </ParallaxLayer>
+
+                {/* 3 */}
+
+                <ParallaxLayer
+                    offset={2}
+                    speed={0}
+                    factor={1}
+                    onClick={() => parallax.current.scrollTo(0)}
+                >
+
+
+                    <div id="LandingPageWebsites" className="LandingPageWebsites">
+                        <div className="LandingPageWebsitesHeader">
+                            <h1>More</h1>
+                            <div className="Marker"></div>
+                        </div>
+
+                        <div className="LandingPageWebsitesDiv">
 
                         </div>
-                        <div className="LandingPageAboutUsFeatureContent">
 
-                        </div>
                     </div>
-                    <div className="LandingPageAboutUsFeature">
-                        <div className="LandingPageAboutUsFeatureHeader">
-
-                        </div>
-                        <div className="LandingPageAboutUsFeatureContent">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* 3 */}
-            
-            <div id="LandingPageWebsites" className="LandingPageWebsites">
-                <div className="LandingPageWebsitesHeader">
-                    <h1>More</h1>
-                    <div className="Marker"></div>
-                </div>
-
-                <div className="LandingPageWebsitesDiv">
-
-                </div>
-
-            </div>
+                </ParallaxLayer>
+            </Parallax>
         </div>
     );
 }
